c88c81e9000a2687bffc9c28fce3b3cab96f73e6
Jessicarv <jessica.ventura.eng@hotmail.com>
2025-07-31 20:45:45 +0000

    Deploying to gh-pages from @ Jessicarv/Jessicarv.github.io@9ff2d5c77c49e6c4d6a8e18467337ac1a366f171 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -479,6 +479,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-incubadora",
+          title: 'Incubadora',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Incubadora/";
             },},{
         id: 'social-email',
         title: 'email',
